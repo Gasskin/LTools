8041a182fbfff6f6f89ce5dc828c8ed3868a1bf2
Gasskin <415753928@qq.com>
2025-08-01 15:42:27 +0800

    轮子

---
+++ Assets/2.SpineGPU优化/Editor/SpineEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class SpineEditor
+{
+    [MenuItem("Assets/LTools/SpineGPU优化/GenMesh")]
+    public static void GenMesh()
+    {
+        var select = Selection.activeGameObject;
+        if (select == null)
+            return;
+        var meshFilter = select.GetComponent<MeshFilter>();
+        if (meshFilter == null)
+            return;
+        var saveMesh = Object.Instantiate(meshFilter.sharedMesh);
+        var saveMeshPath = AssetDatabase.GetAssetPath(select) + $"{select.name}_mesh.asset";
+        AssetDatabase.CreateAsset(saveMesh, saveMeshPath);
+        AssetDatabase.SaveAssets();
+    }
+}
