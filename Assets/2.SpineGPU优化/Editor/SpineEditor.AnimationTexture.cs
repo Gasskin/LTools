b7a6e70fceacb6778a968b63b0a7bc942a11d489
Gasskin <415753928@qq.com>
2025-08-04 22:03:06 +0800

    轮子

--- Assets/2.SpineGPU优化/Editor/SpineEditor.AnimationTexture.cs
+++ Assets/2.SpineGPU优化/Editor/SpineEditor.AnimationTexture.cs
@@ -15,25 +15,33 @@ public partial class SpineEditor
         _meshRenderer = select.GetComponent<MeshRenderer>();
         _skeletonAnimation = select.GetComponent<SkeletonAnimation>();
 
+        var testData = select.GetComponent<TestData>();
+        _skeletonAnimation.loop = false;
         _skeletonAnimation.Initialize(true);
+        // 第0帧
+        AddVertices(testData);
         var animations = _skeletonAnimation.skeletonDataAsset.GetSkeletonData(true).Animations;
         var main = animations.Find((animation => animation.Name == "main"));
         if (main == null)
             return;
         var frameDuration = 1f / 60;
         var frameCount = Mathf.CeilToInt(main.Duration / frameDuration);
-        var testData = select.GetComponent<TestData>();
         testData.Data.Clear();
         Debug.LogError($"frame count:{frameCount}");
         for (int i = 1; i <= frameCount; i++)
         {
-            _skeletonAnimation.Update(i * frameDuration);
+            _skeletonAnimation.Update(frameDuration);
             _skeletonAnimation.LateUpdateMesh();
-            var vertices = _meshFilter.sharedMesh.vertices;
-            var frameVerticesData = new FrameVerticesData();
-            foreach (var v in vertices)
-                frameVerticesData.Vertices.Add(v);
-            testData.Data.Add(frameVerticesData);
+            AddVertices(testData);
         }
+    }
+
+    private static void AddVertices(TestData t)
+    {
+        var vertices = _meshFilter.sharedMesh.vertices;
+        var frameVerticesData = new FrameVerticesData();
+        foreach (var v in vertices)
+            frameVerticesData.Vertices.Add(v);
+        t.Data.Add(frameVerticesData);
     }
 }
--- Assets/2.SpineGPU优化/TestData.cs
+++ Assets/2.SpineGPU优化/TestData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Spine.Unity;
 using TMPro;
 using UnityEngine;
 
@@ -12,14 +13,18 @@ public class FrameVerticesData
 public class TestData : MonoBehaviour
 {
     public List<FrameVerticesData> Data = new();
-    
+
     private MeshFilter _meshFilter;
+    private SkeletonAnimation _skeletonAnimation;
+
     private int _nowFrame;
-    private float _frameDuration = 1 / 30f;
+    private float _frameDuration = 1 / 60f;
+    private float _frameDown = -1f;
     
     private void Awake()
     {
         _meshFilter = GetComponent<MeshFilter>();
+        _skeletonAnimation = GetComponent<SkeletonAnimation>();
     }
 
     private void OnEnable()
@@ -27,20 +32,32 @@ public class TestData : MonoBehaviour
         _nowFrame = 0;
     }
 
-    private void LateUpdate()
+    private void Update()
     {
-        if (_nowFrame >= Data.Count)
-            _nowFrame = 0;
-        var data = Data[_nowFrame];
-        if (_meshFilter != null && _meshFilter.sharedMesh != null)
+        var needUpdate = false;
+        var time = Time.deltaTime;
+        _frameDown -= time;
+        if (_frameDown <= 0)
         {
-            var vertices = _meshFilter.sharedMesh.vertices;
-            for (int i = 0; i < vertices.Length && i < data.Vertices.Count; i++)
-            {
-                vertices[i] = data.Vertices[i];
-            }
-            _meshFilter.sharedMesh.vertices = vertices;
+            needUpdate = true;
         }
-        _nowFrame++;
+        if (needUpdate)
+        {
+            if (_meshFilter != null && _meshFilter.sharedMesh != null)
+            {
+                var vertices = _meshFilter.sharedMesh.vertices;
+                var data = Data[_nowFrame];
+                for (int i = 0; i < vertices.Length && i < data.Vertices.Count; i++)
+                {
+                    vertices[i] = data.Vertices[i];
+                }
+                _meshFilter.sharedMesh.vertices = vertices;
+            }
+            _nowFrame++;
+            if (_nowFrame >= Data.Count)
+            {
+                _nowFrame = 0;
+            }
+        }
     }
 }
